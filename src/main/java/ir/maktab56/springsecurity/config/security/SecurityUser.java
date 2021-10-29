655eb8daa2c648368ee6d64c2a8cdff71a13629f
maxixking66 <1993mohsen1993>
2021-10-29 12:46:38 +0330

    impl UserDetails

---
+++ src/main/java/ir/maktab56/springsecurity/config/security/SecurityUser.java
@@ -0,0 +1,74 @@
+package ir.maktab56.springsecurity.config.security;
+
+import ir.maktab56.springsecurity.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+
+    private User user;
+
+    public SecurityUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(
+                new SimpleGrantedAuthority(
+                        "read"
+                )
+        );
+        return authorities;
+
+        /*return Collections.singletonList(
+                new SimpleGrantedAuthority(
+                        "read"
+                )
+        );*/
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getIsActive();
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
