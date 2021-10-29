0a20fe3fb9d10feb5e4037ac16a6bdc3d41a8034
maxixking66 <1993mohsen1993>
2021-10-29 10:52:55 +0330

    override default config 2

--- src/main/java/ir/maktab56/springsecurity/config/SecurityConfig.java
+++ src/main/java/ir/maktab56/springsecurity/config/SecurityConfig.java
@@ -1,20 +1,38 @@
 package ir.maktab56.springsecurity.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        /*InMemoryUserDetailsManager userDetailsService =
+                new InMemoryUserDetailsManager();
+
+        UserDetails userDetails = User
+                .withUsername("ali")
+                .password("erfagh")
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(userDetails);
+
+        auth.userDetailsService(userDetailsService);*/
+
+        auth.inMemoryAuthentication()
+                .withUser("milad")
+                .password("123456")
+                .authorities("read")
+                .and()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    /*@Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager userDetailsService =
                 new InMemoryUserDetailsManager();
@@ -28,16 +46,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         userDetailsService.createUser(userDetails);
 
         return userDetailsService;
-    }
+    }*/
 
-    @Bean
+    /*@Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }
+    }*/
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
         http.httpBasic();
         http.formLogin();
     }
