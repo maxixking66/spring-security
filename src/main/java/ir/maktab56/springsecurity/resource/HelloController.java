177d6a011b3ef3addb02de31f26c1fc50e2fc055
maxixking66 <1993mohsen1993>
2021-10-29 09:41:44 +0330

    add HelloController

---
+++ src/main/java/ir/maktab56/springsecurity/resource/HelloController.java
@@ -0,0 +1,13 @@
+package ir.maktab56.springsecurity.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
